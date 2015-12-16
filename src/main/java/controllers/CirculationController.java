b413df55f50145a616a04a26e212b5871e6bb0c8
Andriy Slipenko <andrews@interlink-ua.com>
2015-12-16 20:40:49 +0200

    + circulation handler

--- src/main/java/controllers/CirculationController.java
+++ src/main/java/controllers/CirculationController.java
@@ -1,11 +1,16 @@
 package controllers;
 
+import entity.Circulation;
+import entity.Goods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import services.GeneralServiceInterface;
+
+import java.time.LocalDate;
 
 /**
  * Created by ANRI on 16.12.2015.
@@ -20,6 +25,46 @@ public class CirculationController {
     @RequestMapping(method = RequestMethod.GET)
     public String outputCirculations(ModelMap model) {
         model.addAttribute("circulations", generalService.getCirculation());
+        model.addAttribute("goods", generalService.getGoods());
+        model.addAttribute("rooms", generalService.getRooms());
         return "circulation";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String addCirculation(
+            @RequestParam("changeDate") String changeDate,
+            @RequestParam("amount") String amount,
+            @RequestParam("new") String newOrNot,
+            @RequestParam("goodsSelect") String goodsId,
+            @RequestParam("goodsName") String goodsName,
+            @RequestParam("measureValue") String measureValue,
+            @RequestParam("roomSelect") String roomId
+    ) {
+        Circulation circulation = new Circulation();
+
+        circulation.setChangeDate(LocalDate.parse(changeDate));
+        circulation.setChangeAmount(Integer.parseInt(amount));
+
+        if (newOrNot.equals("no")) {
+            circulation.setNewOrNot(0);
+            circulation.setGoods(generalService.getGoodsById(Integer.parseInt(goodsId)));
+        } else {
+            circulation.setNewOrNot(1);
+
+            Goods goods = new Goods();
+
+            goods.setName(goodsName);
+            goods.setAmount(Integer.parseInt(amount));
+            goods.setMeasureValue(measureValue);
+            goods.setRoom(generalService.getRoomsById(Integer.parseInt(roomId)));
+
+            generalService.addGoods(goods);
+
+            circulation.setGoods(goods);
+        }
+
+        generalService.addCirculation(circulation);
+
+        return "redirect:/circulation";
     }
 }
--- web/WEB-INF/views/circulation.jsp
+++ web/WEB-INF/views/circulation.jsp
@@ -45,5 +45,62 @@
     </c:forEach>
 
   </table>
+
+  <div align="center">
+    <form method="post" action="/circulation">
+      <table align="center" class="table">
+        <tr>
+          <td>
+            <label for="changeDate">Дата зміни</label>
+            <input type="date" name="changeDate" id="changeDate" class="form-control">
+          </td>
+          <td>
+            <label for="amount">Кількість</label>
+            <input type="number" name="amount" id="amount" value="0" class="form-control">
+          </td>
+          <td>
+            <label for="newOrNot">Новий</label>
+            <br>
+            <input type="radio" name="new" value="yes" id="newOrNot">Так
+            <br>
+            <input type="radio" name="new" value="no" checked="checked">Ні
+          </td>
+          <td>
+            <label for="goodsSelect">Товар</label>
+            <select size="1" name="goodsSelect" id="goodsSelect" class="form-control">
+              <c:forEach var="good" items="${goods}">
+                <option value="${good.id}">${good.name}</option>
+              </c:forEach>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <label for="goodsName">Назва товару</label>
+            <input type="text" name="goodsName" id="goodsName" value="" class="form-control">
+          </td>
+          <td>
+            <label for="measureValue">Одиниця виміру</label>
+            <input type="text" name="measureValue" id="measureValue" value="" class="form-control">
+          </td>
+          <td>
+            <label for="roomSelect">Кімната</label>
+            <select size="1" name="roomSelect" id="roomSelect" class="form-control">
+              <c:forEach var="room" items="${rooms}">
+                <option value="${room.id}">${room.roomNumber}</option>
+              </c:forEach>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="4" align="right">
+            <input type="submit" name="add" value="Додати"  class="btn btn-default">
+          </td>
+        </tr>
+      </table>
+    </form>
+  </div>
 </body>
 </html>
